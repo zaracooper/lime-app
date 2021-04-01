b06fc2987659a2b98f21bb07523318a1334d5c51
zaracooper <zramcooper@gmail.com>
2021-04-01 20:28:22 +0300

    Add Sku service

---
+++ src/app/data/services/sku.service.ts
@@ -0,0 +1,27 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+import { HttpErrorHandler } from 'src/app/core/services/http-error-handler.service';
+import { Sku } from 'src/app/data/schema/sku';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SkuService {
+  private eh: HttpErrorHandler = new HttpErrorHandler();
+
+  constructor(private http: HttpClient) { }
+
+  getSkus(page: number, pageSize: number): Observable<Sku[]> {
+    let params = new HttpParams();
+    params.append('page', page.toString());
+    params.append('pageSize', pageSize.toString());
+
+    return this.http.get<Sku[]>(
+      `${environment.apiUrl}/api/skus`,
+      { withCredentials: true, params: params })
+      .pipe(catchError(this.eh.handleError));
+  }
+}
