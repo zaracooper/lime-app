830bdbf5e52f5e478dc716fd4d9391780cc6fb70
zaracooper <zramcooper@gmail.com>
2021-04-01 12:30:05 +0300

    Add product list page

--- src/app/modules/products/products.module.ts
+++ src/app/modules/products/products.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ProductListComponent } from './pages/product-list/product-list.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [ProductListComponent],
   imports: [
     CommonModule
   ]
