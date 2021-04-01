167d5624ae7c1ccb651eba7bbf63b1a81aaf63c3
zaracooper <zramcooper@gmail.com>
2021-04-01 20:24:12 +0300

    Refine auth service

--- src/app/core/authentication/authentication.service.ts
+++ src/app/core/authentication/authentication.service.ts
@@ -2,41 +2,34 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
 import { catchError } from 'rxjs/operators';
+import { environment } from 'src/environments/environment';
+import { HttpErrorHandlerService } from '../services/http-error-handler.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthenticationService {
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private eh: HttpErrorHandlerService) { }
+
+  getClientSession(): Observable<object> {
+    return this.http.post<object>(
+      `${environment.apiUrl}/oauth/token`,
+      { grantType: 'client_credentials' },
+      { headers: { 'Content-Type': 'application/json' } })
+      .pipe(catchError(this.eh.handleError));
+  }
 
   login(email: string, password: string): Observable<object> {
-    return this.http.post(
-      '/oauth/token',
-      { email: email, password: password },
+    return this.http.post<object>(
+      `${environment.apiUrl}/oauth/token`,
+      { email: email, password: password, grantType: 'password' },
       { headers: { 'Content-Type': 'application/json' } })
-      .pipe(catchError(this.handleError));
+      .pipe(catchError(this.eh.handleError));
   }
 
   logout(): Observable<object> {
-    return this.http.get('oauth/destroy')
-      .pipe(catchError(this.handleError));
-  }
-
-  session(): Observable<object> {
-    return this.http.get('oauth/session')
-      .pipe(catchError(this.handleError));
-  }
-
-  private handleError(err: HttpErrorResponse): Observable<never> {
-    let errMsg = '';
-
-    if (err.error instanceof ErrorEvent) {
-      errMsg = `A client-side error occurred: ${err.error.message}`;
-    } else {
-      errMsg = `A server-side error occurred. Code: ${err.status}. Message: ${err.message}`;
-    }
-
-    return throwError(errMsg);
+    return this.http.get<object>(`${environment.apiUrl}/session/destroy`)
+      .pipe(catchError(this.eh.handleError));
   }
 }
