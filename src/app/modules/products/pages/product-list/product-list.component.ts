7d4fa639c762841d084ab73df5be73eefae1766b
zaracooper <zramcooper@gmail.com>
2021-04-01 15:38:12 +0300

    Add content to products page

--- src/app/modules/products/pages/product-list/product-list.component.ts
+++ src/app/modules/products/pages/product-list/product-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 
 @Component({
   selector: 'app-product-list',
@@ -6,8 +7,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-list.component.css']
 })
 export class ProductListComponent implements OnInit {
+  rows = 4;
 
-  constructor() { }
+  constructor(breakpointObserver: BreakpointObserver) {
+    breakpointObserver.observe([
+      Breakpoints.Handset,
+      Breakpoints.Tablet,
+      Breakpoints.Web
+    ]).subscribe(result => {
+      if (result.matches) {
+        if (result.breakpoints['(max-width: 599.98px) and (orientation: portrait)']) {
+          this.rows = 1;
+        }
+        else if (result.breakpoints['(min-width: 600px) and (max-width: 839.98px) and (orientation: portrait)']) {
+          this.rows = 2;
+        }
+        else if (result.breakpoints['(min-width: 840px) and (orientation: portrait)']) {
+          this.rows = 4;
+        }
+      }
+    });
+  }
 
   ngOnInit(): void {
   }
