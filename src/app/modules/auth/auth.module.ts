3370a225601882ed69098a35a5c1495876797ec5
zaracooper <zramcooper@gmail.com>
2021-04-01 15:39:28 +0300

    Add material imports to auth module

--- src/app/modules/auth/auth.module.ts
+++ src/app/modules/auth/auth.module.ts
@@ -3,6 +3,10 @@ import { LoginComponent } from './pages/login/login.component';
 import { SignupComponent } from './pages/signup/signup.component';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { RouterModule } from '@angular/router';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
 
 
 
@@ -13,6 +17,10 @@ import { RouterModule } from '@angular/router';
       { path: 'login', component: LoginComponent },
       { path: 'signup', component: SignupComponent }
     ]),
+    MatInputModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatButtonModule,
     SharedModule
   ]
 })
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,16 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatIconModule } from '@angular/material/icon';
-import { MatInputModule } from '@angular/material/input';
-import { MatButtonModule } from '@angular/material/button';
 
 const mods = [
   CommonModule,
-  MatInputModule,
-  MatFormFieldModule,
-  MatIconModule,
-  MatButtonModule,
 ];
 
 @NgModule({
