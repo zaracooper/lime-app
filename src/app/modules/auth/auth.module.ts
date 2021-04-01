5aa229f10acc54d513146eb9eace1b6d108bb2d6
zaracooper <zramcooper@gmail.com>
2021-04-01 10:01:23 +0300

    Add auth module page routes

--- src/app/modules/auth/auth.module.ts
+++ src/app/modules/auth/auth.module.ts
@@ -1,14 +1,19 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { LoginComponent } from './pages/login/login.component';
 import { SignupComponent } from './pages/signup/signup.component';
+import { SharedModule } from 'src/app/shared/shared.module';
+import { RouterModule } from '@angular/router';
 
 
 
 @NgModule({
   declarations: [LoginComponent, SignupComponent],
   imports: [
-    CommonModule
+    RouterModule.forChild([
+      { path: 'login', component: LoginComponent },
+      { path: 'signup', component: SignupComponent }
+    ]),
+    SharedModule
   ]
 })
 export class AuthModule { }
