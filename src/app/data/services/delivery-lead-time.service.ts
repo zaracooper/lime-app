0a231c0273fd4ec509c066b1aae6ee2667ae4c69
zaracooper <zramcooper@gmail.com>
2021-04-03 18:44:02 +0300

    Add method to delivery lead time service

--- src/app/data/services/delivery-lead-time.service.ts
+++ src/app/data/services/delivery-lead-time.service.ts
@@ -1,15 +1,22 @@
-import { HttpClient, HttpParams } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { catchError } from 'rxjs/operators';
 import { HttpErrorHandler } from 'src/app/core/services/http-error-handler.service';
 import { environment } from 'src/environments/environment';
+import { DeliveryLeadTime } from '../schema/delivery-lead-time';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DeliveryLeadTimeService {
   private eh: HttpErrorHandler = new HttpErrorHandler();
+  private url: string = `${environment.apiUrl}/api/delivery-lead-times`;
 
   constructor(private http: HttpClient) { }
+
+  getDeliveryLeadTimes(): Observable<DeliveryLeadTime[]> {
+    return this.http.get<DeliveryLeadTime[]>(this.url)
+      .pipe(catchError(this.eh.handleError));
+  }
 }
