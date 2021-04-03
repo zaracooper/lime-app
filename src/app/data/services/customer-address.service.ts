237d97066b62ca8363d7349f483acf23c9664f74
zaracooper <zramcooper@gmail.com>
2021-04-03 18:39:43 +0300

    Add methods to customer address service

--- src/app/data/services/customer-address.service.ts
+++ src/app/data/services/customer-address.service.ts
@@ -4,12 +4,26 @@ import { Observable } from 'rxjs';
 import { catchError } from 'rxjs/operators';
 import { HttpErrorHandler } from 'src/app/core/services/http-error-handler.service';
 import { environment } from 'src/environments/environment';
+import { CustomerAddress } from '../schema/customer-address';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CustomerAddressService {
   private eh: HttpErrorHandler = new HttpErrorHandler();
+  private url: string = `${environment.apiUrl}/api/customer-addresses`;
 
   constructor(private http: HttpClient) { }
+
+  createCustomerAddress(addressId: string, customerId: string): Observable<CustomerAddress> {
+    return this.http.post<CustomerAddress>(this.url, {
+      addressId: addressId, customerId: customerId
+    })
+      .pipe(catchError(this.eh.handleError));
+  }
+
+  getCustomerAddress(id: string): Observable<CustomerAddress> {
+    return this.http.get<CustomerAddress>(this.url)
+      .pipe(catchError(this.eh.handleError));
+  }
 }
