4bcc400d48b31ca9810459a557024aeffc94f833
zaracooper <zramcooper@gmail.com>
2021-04-03 22:43:51 +0300

    Add product page component

--- src/app/modules/products/products.module.ts
+++ src/app/modules/products/products.module.ts
@@ -7,19 +7,23 @@ import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { LayoutModule } from '@angular/cdk/layout';
+import { ProductComponent } from './pages/product/product.component';
+import { MatIconModule } from '@angular/material/icon';
 
 
 @NgModule({
-  declarations: [ProductListComponent],
+  declarations: [ProductListComponent, ProductComponent],
   imports: [
     RouterModule.forChild([
-      { path: '', component: ProductListComponent }
+      { path: '', component: ProductListComponent },
+      { path: 'product', component: ProductComponent }
     ]),
     CommonModule,
     MatGridListModule,
     MatPaginatorModule,
     MatCardModule,
     MatButtonModule,
+    MatIconModule,
     LayoutModule
   ]
 })
