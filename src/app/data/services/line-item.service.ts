8cd4eec02276f3f94af0cfc0f95794f9967555c4
zaracooper <zramcooper@gmail.com>
2021-04-03 18:52:37 +0300

    Add methods to line item service

--- src/app/data/services/line-item.service.ts
+++ src/app/data/services/line-item.service.ts
@@ -1,15 +1,37 @@
-import { HttpClient, HttpParams } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { catchError } from 'rxjs/operators';
 import { HttpErrorHandler } from 'src/app/core/services/http-error-handler.service';
 import { environment } from 'src/environments/environment';
+import { LineItem } from '../schema/line-item';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LineItemService {
   private eh: HttpErrorHandler = new HttpErrorHandler();
+  private url: string = `${environment.apiUrl}/api/line-items`;
 
   constructor(private http: HttpClient) { }
+
+  createLineItem(lineItem: LineItem): Observable<LineItem> {
+    return this.http.post<LineItem>(this.url, lineItem)
+      .pipe(catchError(this.eh.handleError));
+  }
+
+  getLineItem(id: string): Observable<LineItem> {
+    return this.http.get<LineItem>(this.url)
+      .pipe(catchError(this.eh.handleError));
+  }
+
+  updateLineItem(id: string, quantity: number): Observable<LineItem> {
+    return this.http.patch<LineItem>(this.url, { quantity: quantity, id: id })
+      .pipe(catchError(this.eh.handleError));
+  }
+
+  deleteLineItem(id: string): Observable<LineItem> {
+    return this.http.delete<LineItem>(this.url)
+      .pipe(catchError(this.eh.handleError));
+  }
 }
