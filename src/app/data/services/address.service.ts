4d7f191e7805f93ee386bdba4029069c678f26c5
zaracooper <zramcooper@gmail.com>
2021-04-03 18:35:14 +0300

    Add methods to address service

--- src/app/data/services/address.service.ts
+++ src/app/data/services/address.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Address } from 'node:cluster';
 import { Observable } from 'rxjs';
 import { catchError } from 'rxjs/operators';
 import { HttpErrorHandler } from 'src/app/core/services/http-error-handler.service';
@@ -10,6 +11,17 @@ import { environment } from 'src/environments/environment';
 })
 export class AddressService {
   private eh: HttpErrorHandler = new HttpErrorHandler();
+  private url: string = `${environment.apiUrl}/api/addresses`;
 
   constructor(private http: HttpClient) { }
+
+  createAddress(address: Address): Observable<Address> {
+    return this.http.post<Address>(this.url, address)
+      .pipe(catchError(this.eh.handleError));
+  }
+
+  getAddress(id: string): Observable<Address> {
+    return this.http.get<Address>(this.url)
+      .pipe(catchError(this.eh.handleError));
+  }
 }
