300d8b4b1cde0ebe7f980c607e7e626e0d19411f
zaracooper <zramcooper@gmail.com>
2021-04-03 18:28:39 +0300

    Add data services

--- src/app/data/services/address.service.ts
+++ src/app/data/services/address.service.ts
@@ -4,24 +4,12 @@ import { Observable } from 'rxjs';
 import { catchError } from 'rxjs/operators';
 import { HttpErrorHandler } from 'src/app/core/services/http-error-handler.service';
 import { environment } from 'src/environments/environment';
-import { Sku } from 'src/app/data/schema/sku';
 
 @Injectable({
   providedIn: 'root'
 })
-export class SkuService {
+export class AddressService {
   private eh: HttpErrorHandler = new HttpErrorHandler();
 
   constructor(private http: HttpClient) { }
-
-  getSkus(page: number, pageSize: number): Observable<Sku[]> {
-    let params = new HttpParams();
-    params.append('page', page.toString());
-    params.append('pageSize', pageSize.toString());
-
-    return this.http.get<Sku[]>(
-      `${environment.apiUrl}/api/skus`,
-      { withCredentials: true, params: params })
-      .pipe(catchError(this.eh.handleError));
-  }
 }
