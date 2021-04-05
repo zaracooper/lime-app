19d60b048163c62ee5c4410463232b03406d17ff
zaracooper <zramcooper@gmail.com>
2021-04-05 16:47:30 +0300

    Add shipping address component

--- src/app/modules/checkout/checkout.module.ts
+++ src/app/modules/checkout/checkout.module.ts
@@ -9,13 +9,15 @@ import { AddressComponent } from './components/address/address.component';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatButtonModule } from '@angular/material/button';
 import { BillingAddressComponent } from './pages/billing-address/billing-address.component';
+import { ShippingAddressComponent } from './pages/shipping-address/shipping-address.component';
 
 @NgModule({
-  declarations: [CustomerComponent, TitleComponent, AddressComponent, BillingAddressComponent],
+  declarations: [CustomerComponent, TitleComponent, AddressComponent, BillingAddressComponent, ShippingAddressComponent],
   imports: [
     RouterModule.forChild([
       { path: 'customer', component: CustomerComponent },
-      { path: 'billing-address', component: BillingAddressComponent }
+      { path: 'billing-address', component: BillingAddressComponent },
+      { path: 'shipping-address', component: ShippingAddressComponent }
     ]),
     MatInputModule,
     MatIconModule,
