e3475eed2456cf8e220d6700cfbc319d65987d95
zaracooper <zramcooper@gmail.com>
2021-04-05 14:35:01 +0300

    Add checkout module

---
+++ src/app/modules/checkout/checkout.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CustomerComponent } from './pages/customer/customer.component';
+
+
+
+@NgModule({
+  declarations: [CustomerComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class CheckoutModule { }
