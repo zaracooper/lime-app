f0ef4a689edecd6b7d597a0d1f83f590ecf3dfff
zaracooper <zramcooper@gmail.com>
2021-04-05 18:10:27 +0300

    Add shipping methods component

--- src/app/modules/checkout/checkout.module.ts
+++ src/app/modules/checkout/checkout.module.ts
@@ -8,21 +8,27 @@ import { TitleComponent } from './components/title/title.component';
 import { AddressComponent } from './components/address/address.component';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatButtonModule } from '@angular/material/button';
+import { MatDividerModule } from '@angular/material/divider';
 import { BillingAddressComponent } from './pages/billing-address/billing-address.component';
 import { ShippingAddressComponent } from './pages/shipping-address/shipping-address.component';
+import { ShippingMethodsComponent } from './pages/shipping-methods/shipping-methods.component';
+import { MatRadioModule } from '@angular/material/radio';
 
 @NgModule({
-  declarations: [CustomerComponent, TitleComponent, AddressComponent, BillingAddressComponent, ShippingAddressComponent],
+  declarations: [CustomerComponent, TitleComponent, AddressComponent, BillingAddressComponent, ShippingAddressComponent, ShippingMethodsComponent],
   imports: [
     RouterModule.forChild([
       { path: 'customer', component: CustomerComponent },
       { path: 'billing-address', component: BillingAddressComponent },
-      { path: 'shipping-address', component: ShippingAddressComponent }
+      { path: 'shipping-address', component: ShippingAddressComponent },
+      { path: 'shipping-methods', component: ShippingMethodsComponent }
     ]),
     MatInputModule,
     MatIconModule,
     MatCheckboxModule,
     MatButtonModule,
+    MatDividerModule,
+    MatRadioModule,
     CommonModule
   ]
 })
---
+++ src/app/modules/checkout/pages/shipping-methods/shipping-methods.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-shipping-methods',
+  templateUrl: './shipping-methods.component.html',
+  styleUrls: ['./shipping-methods.component.css']
+})
+export class ShippingMethodsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
