7953a13c9122bee7e9729dd94fca41acbba9e2fa
zaracooper <zramcooper@gmail.com>
2021-04-05 15:53:30 +0300

    Add title component and routes

--- src/app/modules/checkout/checkout.module.ts
+++ src/app/modules/checkout/checkout.module.ts
@@ -1,12 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CustomerComponent } from './pages/customer/customer.component';
-
+import { RouterModule } from '@angular/router';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { TitleComponent } from './components/title/title.component';
 
 
 @NgModule({
-  declarations: [CustomerComponent],
+  declarations: [CustomerComponent, TitleComponent],
   imports: [
+    RouterModule.forChild([
+      { path: 'customer', component: CustomerComponent }
+    ]),
+    MatInputModule,
+    MatIconModule,
     CommonModule
   ]
 })
