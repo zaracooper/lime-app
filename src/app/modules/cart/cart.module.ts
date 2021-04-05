afb482526043bf85a801feeb7a4adbfdc5544c31
zaracooper <zramcooper@gmail.com>
2021-04-05 21:12:39 +0300

    Add a codes component

--- src/app/modules/cart/cart.module.ts
+++ src/app/modules/cart/cart.module.ts
@@ -1,24 +1,30 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { SummaryComponent } from './pages/summary/summary.component';
 import { RouterModule } from '@angular/router';
 import { MatIconModule } from '@angular/material/icon';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDividerModule } from '@angular/material/divider';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { CodesComponent } from './pages/codes/codes.component';
+import { SharedModule } from 'src/app/shared/shared.module';
 
 
 @NgModule({
-  declarations: [SummaryComponent],
+  declarations: [SummaryComponent, CodesComponent],
   imports: [
     RouterModule.forChild([
-      { path: 'cart', component: SummaryComponent }
+      { path: 'cart', component: SummaryComponent },
+      { path: 'codes', component: CodesComponent }
     ]),
     MatIconModule,
     MatMenuModule,
     MatButtonModule,
     MatDividerModule,
-    CommonModule
+    MatFormFieldModule,
+    MatInputModule,
+    SharedModule
   ]
 })
 export class CartModule { }
