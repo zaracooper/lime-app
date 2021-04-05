9f40e287c6868d9aa21af2e7c101c4273cc4ae24
zaracooper <zramcooper@gmail.com>
2021-04-05 16:30:10 +0300

    Add address component

--- src/app/modules/checkout/checkout.module.ts
+++ src/app/modules/checkout/checkout.module.ts
@@ -5,16 +5,21 @@ import { RouterModule } from '@angular/router';
 import { MatInputModule } from '@angular/material/input';
 import { MatIconModule } from '@angular/material/icon';
 import { TitleComponent } from './components/title/title.component';
-
+import { AddressComponent } from './components/address/address.component';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
-  declarations: [CustomerComponent, TitleComponent],
+  declarations: [CustomerComponent, TitleComponent, AddressComponent],
   imports: [
     RouterModule.forChild([
-      { path: 'customer', component: CustomerComponent }
+      { path: 'customer', component: CustomerComponent },
+      { path: 'address', component: AddressComponent }
     ]),
     MatInputModule,
     MatIconModule,
+    MatCheckboxModule,
+    MatButtonModule,
     CommonModule
   ]
 })
