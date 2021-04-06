46916604f2b192c43f88c4e95cfac3bca17fc3e1
zaracooper <zramcooper@gmail.com>
2021-04-06 17:53:43 +0300

    Fix app config

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,20 @@
 import { NgModule, APP_INITIALIZER } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClient, HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { OptionsInterceptor } from './core/interceptor/options.interceptor';
+import { environment } from 'src/environments/environment';
+
+import { CoreModule } from './core/core.module';
+
 import { AuthModule } from './modules/auth/auth.module';
 import { ProductsModule } from './modules/products/products.module';
-import { CoreModule } from './core/core.module';
-import { AuthenticationService } from './core/authentication/authentication.service';
-import { OptionsInterceptor } from './core/interceptor/options.interceptor';
 import { CartModule } from './modules/cart/cart.module';
-import { Logger } from './core/services/logger.service';
 import { CheckoutModule } from './modules/checkout/checkout.module';
-
-
-function getSession(authService: AuthenticationService) {
-  authService.getClientSession().subscribe(
-    () => { },
-    err => new Logger('App initialization').warn('Failed to get session.', err));
-}
 
 @NgModule({
   declarations: [
@@ -41,13 +36,17 @@ function getSession(authService: AuthenticationService) {
       provide: HTTP_INTERCEPTORS,
       useClass: OptionsInterceptor,
       multi: true
+    },
+    {
+      provide: APP_INITIALIZER,
+      useFactory: (http: HttpClient) => () => http.post<object>(
+        `${environment.apiUrl}/oauth/token`,
+        { 'grantType': 'client_credentials' },
+        { withCredentials: true })
+        .toPromise(),
+      multi: true,
+      deps: [HttpClient]
     }
-    // {
-    //   provide: APP_INITIALIZER,
-    //   useFactory: getSession,
-    //   multi: true,
-    //   deps: [AuthenticationService]
-    // }
   ],
   bootstrap: [AppComponent]
 })
