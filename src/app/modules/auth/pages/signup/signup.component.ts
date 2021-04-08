715d1bfde5725390141e35c03f8548737355381e
zaracooper <zramcooper@gmail.com>
2021-04-09 02:40:28 +0300

    Add functionality to signup form

--- src/app/modules/auth/pages/signup/signup.component.ts
+++ src/app/modules/auth/pages/signup/signup.component.ts
@@ -1,15 +1,63 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { AbstractControl, FormBuilder, FormGroup, FormGroupDirective, ValidationErrors, Validators } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
+import { UntilDestroy } from '@ngneat/until-destroy';
+import { CustomerService } from 'src/app/data/services/customer.service';
 
+@UntilDestroy({ checkProperties: true })
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
   styleUrls: ['./signup.component.css']
 })
-export class SignupComponent implements OnInit {
+export class SignupComponent {
+  signupForm = this._fb.group({
+    firstName: ['', Validators.required],
+    lastName: ['', Validators.required],
+    email: ['', Validators.required],
+    password: ['', [Validators.required, Validators.minLength(8)]],
+    confirmedPassword: ['', [Validators.required]]
+  }, { validators: this.matchPasswords });
 
-  constructor() { }
+  @ViewChild(FormGroupDirective) sufDirective: FormGroupDirective | undefined;
 
-  ngOnInit(): void {
+  constructor(
+    private _customer: CustomerService,
+    private _fb: FormBuilder,
+    private _snackBar: MatSnackBar,
+    private _router: Router) { }
+
+  matchPasswords(signupGroup: AbstractControl): ValidationErrors | null {
+    const password = signupGroup.get('password')?.value;
+    const confirmedPassword = signupGroup.get('confirmedPassword')?.value;
+
+    return password == confirmedPassword ? null : { differentPasswords: true };
   }
 
+  get password() { return this.signupForm.get('password'); }
+
+  get confirmedPassword() { return this.signupForm.get('confirmedPassword'); }
+
+  signup() {
+    const customer = this.signupForm.value;
+
+    this._customer.createCustomer(
+      customer.email,
+      customer.password,
+      customer.firstName,
+      customer.lastName
+    ).subscribe(
+      () => {
+        this.signupForm.reset();
+        this.sufDirective?.resetForm();
+
+        this._snackBar.open('Account successfully created. You will be redirected in 5 seconds.', 'Close', { duration: 5000 });
+
+        setTimeout(() => this._router.navigateByUrl('/'), 6000);
+
+      },
+      err => this._snackBar.open('There was a problem creating your account.', 'Close', { duration: 5000 })
+    );
+  }
 }
