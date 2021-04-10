d54a750c34cbfb8432e5ea3405705c4235bbf002
zaracooper <zramcooper@gmail.com>
2021-04-11 00:01:40 +0300

    Add empty cart guard

---
+++ src/app/core/guards/empty-cart.guard.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { CartService } from 'src/app/data/services/cart.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EmptyCartGuard implements CanActivate {
+  constructor(private _cart: CartService, private _router: Router) { }
+
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    if (this._cart.orderId) {
+      if (this._cart.itemCount > 0) {
+        return true
+      }
+    }
+
+    return this._router.parseUrl('/empty');
+  }
+
+}
