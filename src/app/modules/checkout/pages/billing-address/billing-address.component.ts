d693a5737623eeac9eae6c7072ac9c998eb9690a
zaracooper <zramcooper@gmail.com>
2021-04-12 14:58:54 +0300

    Add address list to billing address component

--- src/app/modules/checkout/pages/billing-address/billing-address.component.ts
+++ src/app/modules/checkout/pages/billing-address/billing-address.component.ts
@@ -1,15 +1,76 @@
 import { Component, OnInit } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { UntilDestroy } from '@ngneat/until-destroy';
+import { of } from 'rxjs';
+import { mergeMap } from 'rxjs/operators';
+import { SessionService } from 'src/app/core/authentication/session.service';
+import { Address } from 'src/app/data/schema/address';
+import { CustomerAddress } from 'src/app/data/schema/customer-address';
+import { UpdateOrderParams } from 'src/app/data/schema/order';
+import { AddressService } from 'src/app/data/services/address.service';
+import { CartService } from 'src/app/data/services/cart.service';
+import { CustomerAddressService } from 'src/app/data/services/customer-address.service';
+import { OrderService } from 'src/app/data/services/order.service';
 
+@UntilDestroy({ checkProperties: true })
 @Component({
   selector: 'app-billing-address',
   templateUrl: './billing-address.component.html',
   styleUrls: ['./billing-address.component.css']
 })
 export class BillingAddressComponent implements OnInit {
+  addresses: CustomerAddress[] = [];
+  showAddresses: boolean = false;
+  sameShippingOrBillingAddress: boolean = false;
 
-  constructor() { }
+  constructor(
+    private _addresses: AddressService,
+    private _customerAddresses: CustomerAddressService,
+    private _snackBar: MatSnackBar,
+    private _session: SessionService,
+    private _orders: OrderService,
+    private _cart: CartService) { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this._session.loggedInStatus
+      .subscribe(
+        status => this.showAddresses = status
+      );
   }
 
+  addAddress(address: Address) {
+    if (this.showAddresses) {
+
+    }
+    else {
+
+    }
+  }
+
+  private createAddress(address: Address) {
+    this._addresses.createAddress(address)
+      .pipe(
+        mergeMap(address => {
+          let updateParams = [UpdateOrderParams.billingAddress];
+          if (this.sameShippingOrBillingAddress) {
+            updateParams.push(UpdateOrderParams.shippingAddressSameAsBilling);
+          }
+
+          return this._orders.updateOrder({
+            id: this._cart.orderId,
+            billingAddressId: address.id
+          }, updateParams);
+        })
+      )
+      .subscribe({
+        error: err => this._snackBar.open('There was a problem creating your address.', 'Close', { duration: 800 })
+      });
+  }
+
+  private cloneAddress(id: string) {
+
+  }
+
+  private updateOrderObservable() {
+  }
 }
