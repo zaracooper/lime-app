f22f60b34e14efee9487d428f66b567a5e4bdd94
zaracooper <zramcooper@gmail.com>
2021-04-12 06:02:41 +0300

    Add a country select component

---
+++ src/app/modules/checkout/components/country-select/country-select.component.ts
@@ -0,0 +1,32 @@
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { UntilDestroy } from '@ngneat/until-destroy';
+import { Country } from 'src/app/data/schema/country';
+import { CountryService } from 'src/app/data/services/country.service';
+
+@UntilDestroy({ checkProperties: true })
+@Component({
+  selector: 'app-country-select',
+  templateUrl: './country-select.component.html',
+  styleUrls: ['./country-select.component.css']
+})
+export class CountrySelectComponent implements OnInit {
+  country: string = 'Country';
+  countries: Country[] = [];
+  @Output() setQuantityEvent = new EventEmitter<string>();
+
+  constructor(private _countries: CountryService) { }
+
+  ngOnInit() {
+    this._countries.getCountries()
+      .subscribe(
+        countries => {
+          this.countries = countries;
+        }
+      );
+  }
+
+  setCountry(value: Country) {
+    this.country = value.name || '';
+    this.setQuantityEvent.emit(value.code);
+  }
+}
