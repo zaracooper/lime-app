8b70dc86d1c037c573740bc25dac75f44d561f35
zaracooper <zramcooper@gmail.com>
2021-04-12 14:57:56 +0300

    Add form group to address component

--- src/app/modules/checkout/components/address/address.component.ts
+++ src/app/modules/checkout/components/address/address.component.ts
@@ -1,4 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { Address } from 'src/app/data/schema/address';
 
 @Component({
   selector: 'app-address',
@@ -6,12 +8,42 @@ import { Component, Input, OnInit } from '@angular/core';
   styleUrls: ['./address.component.css']
 })
 export class AddressComponent implements OnInit {
-  @Input() showCheckbox: boolean = true;
   @Input() buttonText: string = '';
+  @Input() showTitle?: boolean = false;
+  @Input() createAddress!: (address: Address) => void;
 
-  constructor() { }
+  countryCode: string = '';
+
+  addressForm = this._fb.group({
+    firstName: ['', [Validators.required]],
+    lastName: ['', [Validators.required]],
+    line1: ['', [Validators.required]],
+    city: ['', [Validators.required]],
+    zipCode: ['', [Validators.required]],
+    stateCode: ['', [Validators.required]],
+    phone: ['', [Validators.required]]
+  });
+
+  constructor(private _fb: FormBuilder) { }
 
   ngOnInit(): void {
+  }
+
+  setCountryCode(code: string) {
+    this.countryCode = code;
+  }
+
+  addAddress() {
+    this.createAddress({
+      firstName: this.addressForm.get('firstName')?.value,
+      lastName: this.addressForm.get('lastName')?.value,
+      line1: this.addressForm.get('line1')?.value,
+      city: this.addressForm.get('city')?.value,
+      zipCode: this.addressForm.get('zipCode')?.value,
+      stateCode: this.addressForm.get('stateCode')?.value,
+      countryCode: this.addressForm.get('countryCode')?.value,
+      phone: this.addressForm.get('phone')?.value
+    });
   }
 
 }
